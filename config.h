e9012ef636cdec20f378f9720fe428f068c2b575
Hamed <hamedbk@getgoogleoff.me>
2025-11-13 11:43:36 +0100

    Add disk free & used space. +better formatting with colons and 2 letters

--- config.h
+++ config.h
@@ -66,12 +66,14 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument turn signal */
-    { keymap,   "  %s"  , NULL, 1,    3 },                            
-	{ ram_free, " FM%s" , NULL, 1,   -1 },
-	{ ram_used, " UM%s" , NULL, 1,   -1 },
-	{ cpu_freq, " F%s"  , NULL, 1,   -1 },
-	{ cpu_perc, " C%s%%", NULL, 1,   -1 },
-    { run_command,  " V%s%",  "bash -c 'pamixer --get-mute | grep true &>/dev/null && echo M || pamixer --get-volume'", -1, 2 },             
+    { keymap,   "  KM:%s"  , NULL, 1,    3 },
+	{ disk_free, " DF:%s" , "/", 1,   -1 },
+	{ disk_used, " DU:%s" , "/", 1,   -1 },
+	{ ram_free, " RF:%s" , NULL, 1,   -1 },
+	{ ram_used, " RU:%s" , NULL, 1,   -1 },
+	{ cpu_freq, " CF:%s"  , NULL, 1,   -1 },
+	{ cpu_perc, " CU:%s%%", NULL, 1,   -1 },
+    { run_command,  " V:%s%",  "bash -c 'pamixer --get-mute | grep true &>/dev/null && echo M || pamixer --get-volume'", -1, 2 },
 	{ datetime, " %s",  "%Y %b %d %a %T", 1,   -1 },
 };
 
